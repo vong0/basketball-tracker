d69f6be748722e4fa0596bf60c8ea97346a79181
mvong <matthewvong23@gmail.com>
2026-06-13 15:33:44 +1000

    update vite so can test locally on mobile connected to same network as server

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/basketball-tracker/',  // <-- your repo name, with leading + trailing slashes
+  base: '/basketball-tracker/',
+  server: {
+    host: true,  // or use '0.0.0.0'
+    port: 5173
+  }
 });
